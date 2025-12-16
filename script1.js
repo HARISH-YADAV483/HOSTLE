3eac3d54c846bb82178cbeead03df3e6d73c159d
HARISH-YADAV483 <harish4nitkkr@gmail.com>
2025-12-16 23:53:08 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -4,7 +4,7 @@ const sidebarDiv = document.getElementById('toggleDiv');
 
 img.onclick = function() {
   if (sidebarDiv.style.display === 'none' || sidebarDiv.style.display === '') {
-    sidebarDiv.style.display = 'block';
+    sidebarDiv.style.display = 'flex';
   } else {
     sidebarDiv.style.display = 'none';
   }
